093e28a345701438717cd881b3d32afc96c5aeac
Evan Murray <emurray49@gatech.edu>
2024-11-15 13:59:30 -0500

    Move everything into one cpp file

--- concepts/spectral_synthesis/supercollidercamera/plugins/Camera/Camera.cpp
+++ concepts/spectral_synthesis/supercollidercamera/plugins/Camera/Camera.cpp
@@ -1,35 +1,41 @@
 // PluginCamera.cpp
 // Evan Murray (emurray49@gatech.edu)
-
 #include "SC_PlugIn.hpp"
-#include "Camera.hpp"
+#include <opencv2/objdetect.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/videoio.hpp>
+#include <iostream>
+#include <iomanip>
 
 static InterfaceTable* ft;
 
 namespace SuperColliderCamera {
-
-Camera::Camera() {
-    mCalcFunc = make_calc_function<Camera, &Camera::next>();
-    next(1);
-}
-
-void Camera::next(int nSamples) {
-
-    // Audio rate input
-    const float* input = in(0);
-
-    // Control rate parameter: gain.
-    const float gain = in0(1);
-
-    // Output buffer
-    float* outbuf = out(0);
-
-    // simple gain function
-    for (int i = 0; i < nSamples; ++i) {
-        outbuf[i] = input[i] * gain;
+class Camera : public SCUnit {
+public:
+    Camera()
+    {
+        mCalcFunc = make_calc_function<Camera, &Camera::next>();
+        next(1);
     }
-}
+private:
+    void next(int nSamples)
+    {
+        // Audio rate input
+        const float* input = in(0);
 
+        // Control rate parameter: gain.
+        const float gain = in0(1);
+
+        // Output buffer
+        float* outbuf = out(0);
+
+        // simple gain function
+        for (int i = 0; i < nSamples; ++i) {
+            outbuf[i] = input[i] * gain;
+        }
+    }
+};
 } // namespace SuperColliderCamera
 
 PluginLoad(CameraUGens) {
